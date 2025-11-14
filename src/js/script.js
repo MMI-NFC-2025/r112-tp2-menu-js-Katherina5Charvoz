85abda94e00fd9acd46b63a637bbf05c959eabfd
Katherina5Charvoz <kathcha.ck@gmail.com>
2025-11-14 11:56:53 +0100

    -

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+
+// Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    
+  // Mise à jour des attributs ARIA pour accessibilité
+  nav.ariaHidden = isOpen;
+  toggle.ariaExpanded = isClosed;
+  document.body.classList.toggle("noscroll")
+});
